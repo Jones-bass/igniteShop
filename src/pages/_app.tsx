23f696bfb1f649cef60d6ff32253e0067d7a9c84
jones-bass <jonesbass.tb@gmail.com>
2022-10-04 17:15:37 -0300

    Config Stylus Globais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
+import { AppProps } from "next/app";
+import { globalCss } from "../styles";
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
+  globalCss();
+  
   return <Component {...pageProps} />
 }
 
-export default MyApp
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
+import { styled } from "../styles";
 
- function Home() {
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold'
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  }
+})
+
+function Home() {
   return (
-    <div>
+    <Button>
       Ola BB
-    </div>
+    </Button>
   )
 }
 
