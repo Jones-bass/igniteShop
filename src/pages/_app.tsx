9c663b933405a2ea5a6992ea9fad9e358a310287
jones-bass <jonesbass.tb@gmail.com>
2022-10-04 22:17:38 -0300

    Estruturando Pagina Home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,23 @@
 import { AppProps } from "next/app";
 import { globalCss } from "../styles";
 
+import logoImg from "../assets/logo.svg"
+import Image from "next/future/image"
+
+import { Container, Header } from "../styles/pages/app"
+
 export default function App({ Component, pageProps }: AppProps) {
   globalCss();
   
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 
